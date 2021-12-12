337142aad2c0ce43df743a50ab0ad639bc70b8d8
kisslorand <kisslorand666@yahoo.com>
2021-12-12 10:39:37 +0200

    E-step Z-current

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -926,7 +926,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.096, 80.162, 400, 817 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.096, 80.162, 400, 809 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2673,7 +2673,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       800
+    #define Z_CURRENT       600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2682,7 +2682,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      800
+    #define Z2_CURRENT      600
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.11
