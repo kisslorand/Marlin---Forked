f0e4fe703296ebe97984d09985bccc9fc6331035
kisslorand <kisslorand666@yahoo.com>
2022-05-29 12:58:44 +0300

    Z Offset for BIQU H2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1279,7 +1279,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 25, -18, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 35, -27.5, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
